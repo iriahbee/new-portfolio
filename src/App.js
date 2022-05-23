d2ec9f903075b501409ed6bdf7d086db261b1a71
iriahbee <solagnelake@gmail.com>
2022-05-23 22:31:54 +0100

    New Nav

--- src/App.js
+++ src/App.js
@@ -1,4 +1,7 @@
 import React from "react";
+import GlobalStyle from './GlobalStyles';
+
+//Page Sections
 import Footer from "./components/Footer";
 import Scrolltotop from "./components/Scrolltotop";
 import About from "./pages/About";
